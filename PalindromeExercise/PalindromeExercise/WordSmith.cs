616017c668ac918b1370e882f913999a22eebb33
Myles Hill <mylehill99@gmail.com>
2023-12-08 12:53:30 -0500

    finished

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+
+        public bool IsAPalindrome(string input) 
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            var charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+
+            return new string(charArray) == input;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,22 @@
+using PalindromeExercise;
 using System;
+using System.ComponentModel.DataAnnotations;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("test", false)]
+        public void TestIsAPalindrome(string input, bool expected)
         {
+            var wordSmith = new WordSmith();
 
+            var actual = wordSmith.IsAPalindrome(input);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
